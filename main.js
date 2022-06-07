9c897b9d015ca14364cfd1adf5af3493de672628
ronald-it <ronald.cirgofer@hotmail.com>
2022-06-07 10:15:34 +0200

    gitignore aangepast en stappenplan in html css en js codevelden geplaatst

---
+++ main.js
@@ -0,0 +1,17 @@
+// SCRIPT
+
+// 1. Sla de referentie naar het gebruikersnaam-veld op
+
+// 2. Sla de referentie naar het gebruikersnaam-waarschuwing element op
+
+// 3. Zet een event listener op het gebruikersnaam-veld
+
+// 4. Maak een functie die de event listener kan aanroepen als de gebruiker input geeft
+
+// 5. Laat die functie checken of de huidige waarde van de input een '@' bevat
+
+// 6. Als dit zo is, geef de waarschuwing weer
+
+// 7. Als dit niet zo is, zorg dan dat de waarschuwing verdwijnt
+
+// 8. Herhaal deze stappen voor het wachtwoord
